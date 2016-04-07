b3daa0cc074a9074a5bfa967895a08675263394c
Gaston Ramos <ramos.gaston@gmail.com>
2016-04-07 13:07:04 -0300

    Add new exercises.

---
+++ nodejs/node-js-the-right-way/requestHandlers.js
@@ -0,0 +1,30 @@
+function iniciar(response) {
+  console.log("Manipulador de peticiones 'iniciar' fue llamado.");
+
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" content="text/html; 
+        charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/subir" method="post">'+
+    '<textarea name="text" rows="20" cols="60"></textarea>'+
+    '<input type="submit" value="Enviar texto" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
+
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(body);
+    response.end();
+}
+
+function subir(response) {
+  console.log("Manipulador de peticiones 'subir' fue llamado.");
+  response.writeHead(200, {"Content-Type": "text/html"});
+  response.write("Hola Subir");
+  response.end();
+}
+
+exports.iniciar = iniciar;
+exports.subir = subir;
