6335e96da85f2b73c039b6d5d03fb4cb28d7a2e7
Gaston Ramos <ramos.gaston@gmail.com>
2016-04-21 09:15:24 -0300

    add database exercises.

---
+++ nodejs/node-js-the-right-way/database/lib/rdf-parser.js
@@ -0,0 +1,26 @@
+'use strict';
+const
+fs = require('fs'),
+cheerio = require('cheerio');
+
+module.exports = function(filename, callback) {
+    fs.readFile(filename, function(err, data){
+        if (err) { return callback(err); }
+        let
+        $ = cheerio.load(data.toString()),
+
+        collect = function(index, elem) {
+            //return ['hola'];
+            console.log( $(elem).text() );
+            return $(elem).text();
+        };
+
+        callback(null, {
+            _id: $('pgterms\\:ebook').attr('rdf:about').replace('ebooks/', ''),
+            title: $('dcterms\\:title').text(),
+            authors: $('pgterms\\:name').map(collect).get(),
+            subjects: $('[rdf\\:resource$="/LCSH"]').parent()
+                .children('rdf\\:value').map(collect).get()
+        });
+    });
+};
